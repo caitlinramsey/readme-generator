875359daef9fb33c1d5f58145e330b2813e79979
Caitlin Ramsey <caitlinramsey@Caitlins-MacBook-Pro.local>
2023-05-12 11:59:52 -0400

    added questions and fs writeToFile

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = ['What was your motivation?', 'Why did you build this project?', 
-'What problem does it solve?', 'What did you learn?', 'What makes your project stand out?'];
 
+inquirer   
+    .prompt([
+        {
+            type: 'input',
+            name: 'motivation',
+            message: 'What was your motivation?',
+        },
+        {
+            type: 'input',
+            name: 'build',
+            message: 'Why did you build this project?',
+        },
+        {
+            type: 'input',
+            name: 'problem',
+            message: 'What problem does it solve?',
+        },
+        {
+            type: 'input',
+            name: 'learn',
+            message: 'What did you learn?',
+        },
+        {
+            type: 'input',
+            name: 'standout',
+            message: 'What makes your project stand out?',
+        },
+    ])
+    .then((answers) => {
+        const readMeContent = generateREADME(answers);
+
+        fs.writeToFile('README.md', readMeContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README!')
+        );
+    });
+    
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     return fileName + questions + data
